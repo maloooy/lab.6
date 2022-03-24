d3fa692defa8961533399a5a5be48ca6922f5766
Mikhail <basargin123@gmail.com>
2022-03-24 20:47:12 +0700

    Добавьте файлы проекта.

---
+++ lab6/Models/ToDoList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6.Models
+{
+    public class ToDoList
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public ToDoList(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+        }
+    }
+}
---
+++ lab6/Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+
+
+namespace lab6.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
